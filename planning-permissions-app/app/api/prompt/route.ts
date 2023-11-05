41f89a1ec47e1575c22c2f869b66fc34366a3e63
rowan-aldean <rowanaldean@gmail.com>
2023-11-05 10:15:36 +0000

    wip: template vars

--- planning-permissions-app/app/api/prompt/route.ts
+++ planning-permissions-app/app/api/prompt/route.ts
@@ -1,18 +1,12 @@
 // langchain imports
 import { PromptTemplate } from "langchain/prompts";
 import { ChatAnthropic } from "langchain/chat_models/anthropic";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { formatDocumentsAsString } from "langchain/util/document";
+import { SimpleSequentialChain, LLMChain } from "langchain/chains";
 // unrelated imports
 import { NextRequest, NextResponse } from "next/server";
 import { queryDatabaseForPDFs } from "@/lib/database";
 
-
 export async function POST(request: NextRequest) {
-  // TOOD: Delete this when no longer loading from static files
-  // const loader = new PDFLoader(".\\public\\pdfs\\example-planning-doc.pdf", {
-  //   splitPages: false,
-  // });
 
   // Create an instance of Langchain with your Anthropi API key
   const model = new ChatAnthropic({
@@ -26,33 +20,68 @@ export async function POST(request: NextRequest) {
   const usersProjectJson = await request.json();
   const usersProjectMessage = usersProjectJson.messageQuery;
 
-  // TODO: The chaining needs to be done
-
   // Prompt Claude 2 using Langchain
-  const promptTemplate = PromptTemplate.fromTemplate(
-    `Human: <council-rules-collection>{datasource}</council-rules-collection> 
-    
-    You are an advisor on UK planning permissions for construction projects. You have been provided with a large datasource of rules for each council.
-    A client of yours has asked you about the following project they plan on undertaking:
+  const roleTemplateInput = `
+  Human: You are an advisor on UK planning permissions for residential and commercial projects. Your primary task
+  is to determine if a user needs planning permission or not based on the local council for the area which the project is
+  intended to take place. An example is that most home extensions of significant measure need permissions.
 
-    <potential-project>{userMessage}</potential-project>
+  You are expected to always be concise and to use the data provided and at your disposal to determine as quickly as possible
+  whether a user needs planning permission for the given project they describe to you.
 
-    Considering what council they fall within based on the area of their home, you have been asked to provide a series of questions
-    to qualify if they may need planning permission.
-    
-    Skip the preamble and reply to them in the most clear and concise way, using the council rules to establish if they even need permission in the least number of questions.
+  Assistant:
+  `;
+  const roleTemplate = PromptTemplate.fromTemplate(roleTemplateInput);
 
-    Assistant: For the project request {userMessage} I'll need to know...
-    `
-  );
+  const roleChain = new LLMChain({ llm: model, prompt: roleTemplate });
 
-  const chain = promptTemplate.pipe(model);
+  // ROLE PART OF THE CHAIN CONFIGURED
 
-  const stringDatadump = datadump?.map(doc => doc.content).join('') || ''; //TODO: Have some visibility for if this is null? Will Claude realise?
+  const usersProjectInput = `
+  Human: <council-rules-collection>{datasource}</council-rules-collection>
 
-  const result = await chain.invoke({ datasource: stringDatadump, userMessage: usersProjectMessage });
+  A user has come to you with the following project:
+
+  <users-project-description>{userMessage}</users-project-description>
+
+  Recall that your job is to use all data at your disposal. You have been provided with a collection of council rules and supporting documentation.
+  Use this as a reference to determine if the project outlined by the user requires planning permission or not considering what council the project will take place in.
+
+  You have been asked to provide a series of questions to qualify if they may need planning permission.
+
+  Until you have determined whether or not they need permission, ask a single question to them and await a response.
+  At any point during the conversation, if you have determined that they do not require planning permission then stop asking questions
+  and return the exact term <not-required-term>NO-PERMISSION-REQUIRED</not-required-term>.
+  
+  If during your process of asking single questions to the user, you determine that they do require planning permission then return the 
+  exact term <required-term>PERMISSION-REQUIRED.</required-term>
+
+  I as a developer will integrate my code to look for these terms. It is paramount that you use them specifically as described above.
+
+  Assistant:
+  `;
+
+  const usersProjectTemplate =  new PromptTemplate({
+    template: usersProjectInput,
+    inputVariables: ["datasource", "userMessage"],
+  });
+
+  const usersProjectChain = new LLMChain({ llm: model, prompt: usersProjectTemplate });
+
+  const overallChain = new SimpleSequentialChain({
+    chains: [roleChain, usersProjectChain],
+    verbose: true,
+  });
+
+  const stringDatadump = datadump?.map((doc) => doc.content).join("") || ""; //TODO: Have some visibility for if this is null? Will Claude realise?
+
+  const result = await overallChain.invoke({
+    datasource: stringDatadump,
+    userMessage: usersProjectMessage,
+  });
 
   // console.log(result);
   // Return the response
+  // result.content = 
   return NextResponse.json(result.content);
 }
